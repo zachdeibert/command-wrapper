1e7e01682503481baf3323e68cc3ed31443e3d4e
Zach Deibert <zachdeibert@gmail.com>
2016-11-08 21:25:26 -0600

    Fixed MSBuild Task

--- CommandTask.cs
+++ CommandTask.cs
@@ -3,14 +3,20 @@ using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace commandwrapper {
-	public class MSBuildTask : Task {
+	public class CommandTask : Task {
 		[Required]
 		public string Command {
 			get;
 			set;
 		}
+			
+		public string WorkingDirectory {
+			get;
+			set;
+		}
 
 		public override bool Execute() {
+			Environment.CurrentDirectory = WorkingDirectory;
 			return MainClass.Run(Command.Split(' ')) == 0;
 		}
 	}

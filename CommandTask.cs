63759a783475d0d37045bfad2a77c6564598b38a
Zach Deibert <zachdeibert@gmail.com>
2017-05-26 08:42:55 -0500

    Added support for argument quoting

--- CommandTask.cs
+++ CommandTask.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -15,11 +17,51 @@ namespace commandwrapper {
 			set;
 		}
 
+		public static string[] SplitCommandLine(string argString) {
+			List<string> args = new List<string>();
+			StringBuilder arg = new StringBuilder();
+			bool singleQuoted = false;
+			bool doubleQuoted = false;
+			foreach (char c in argString) {
+				switch (c) {
+					case '"':
+						if (singleQuoted) {
+							arg.Append('"');
+						} else {
+							doubleQuoted = !doubleQuoted;
+						}
+						break;
+					case '\'':
+						if (doubleQuoted) {
+							arg.Append('\'');
+						} else {
+							singleQuoted = !singleQuoted;
+						}
+						break;
+					case ' ':
+						if (singleQuoted || doubleQuoted) {
+							arg.Append(' ');
+						} else {
+							args.Add(arg.ToString());
+							arg.Clear();
+						}
+						break;
+					default:
+						arg.Append(c);
+						break;
+				}
+			}
+			if (arg.Length > 0) {
+				args.Add(arg.ToString());
+			}
+			return args.ToArray();
+		}
+
 		public override bool Execute() {
 			string oldcwd = Environment.CurrentDirectory;
 			try {
 				Environment.CurrentDirectory = WorkingDirectory;
-				return MainClass.Run(Command.Split(' ')) == 0;
+				return MainClass.Run(SplitCommandLine(Command)) == 0;
 			} finally {
 				Environment.CurrentDirectory = oldcwd;
 			}
